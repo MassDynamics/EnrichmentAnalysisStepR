f8aaf6572553daaa73875604ac765e128c2ba260
Joseph Isaac Bloom <joseph@massdynamics.com>
2021-05-11 11:11:12 +1000

    fix dep script

--- inst/install_dependencies.r
+++ inst/install_dependencies.r
@@ -6,16 +6,13 @@ list.of.packages <- c(
   "jsonlite",
   "tidyr",
   "dplyr",
-  "EnrichmentBrowser",
   "stringr",
   "R.utils",
+  "BiocManager"
 )
 
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages, repos = "http://cran.us.r-project.org")
 
-library(devtools)
 
-#expects to be run from .. (home dir)
-devtools::install_deps(pkg = ".", upgrade = "never", repos = "http://cran.us.r-project.org")
-
+BiocManager::install("EnrichmentBrowser")
