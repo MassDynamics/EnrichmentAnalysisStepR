a7872892aa199607ce82e90015935163c78dc52d
Joseph Isaac Bloom <joseph@massdynamics.com>
2021-05-11 11:03:21 +1000

    write workflow dependency script

---
+++ inst/install_dependencies.r
@@ -0,0 +1,21 @@
+# production environment install script
+
+cat("Preparing R enviroment")
+
+list.of.packages <- c(
+  "jsonlite",
+  "tidyr",
+  "dplyr",
+  "EnrichmentBrowser",
+  "stringr",
+  "R.utils",
+)
+
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages, repos = "http://cran.us.r-project.org")
+
+library(devtools)
+
+#expects to be run from .. (home dir)
+devtools::install_deps(pkg = ".", upgrade = "never", repos = "http://cran.us.r-project.org")
+
