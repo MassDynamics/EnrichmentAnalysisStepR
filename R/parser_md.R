cd4265581d2743eceb14d755acaf7f6a11a872bd
Joseph Isaac Bloom <joseph@massdynamics.com>
2021-05-12 07:35:21 +1000

    filter columns by name not index

--- R/parser_md.R
+++ R/parser_md.R
@@ -109,7 +109,7 @@ process_protein_intensities_by_protein <- function(protein_count_and_intensities
 
   protein_count_and_intensities =  unnest(protein_count_and_intensities,cols = c(conditions)) #first level unwrap
   protein_count_and_intensities =  unnest(protein_count_and_intensities,cols = c(intensityValues)) #second level unwrap
-  protein_count_and_intensities = protein_count_and_intensities[,c(2,8,14)]
+  protein_count_and_intensities = protein_count_and_intensities[,c("ProteinId", "name", "log2NInt_ProteinGroupId")]
   protein_count_and_intensities = protein_count_and_intensities %>% group_by(ProteinId,name)  %>% mutate(replicate = row_number())
   protein_count_and_intensities = protein_count_and_intensities %>%
     group_by(ProteinId) %>%
@@ -125,7 +125,7 @@ process_protein_intensities_by_gene <- function(protein_count_and_intensities){
   protein_count_and_intensities =  unnest(protein_count_and_intensities,cols = c(conditions)) #first level unwrap
   protein_count_and_intensities =  unnest(protein_count_and_intensities,cols = c(intensityValues)) #second level unwrap
   print(colnames(protein_count_and_intensities))
-  protein_count_and_intensities = protein_count_and_intensities[,c(2,3,8,14)]
+  protein_count_and_intensities = protein_count_and_intensities[,c("ProteinId", "GeneName", "name", "log2NInt_ProteinGroupId")]
   protein_count_and_intensities = protein_count_and_intensities %>% group_by(GeneName, ProteinId,name)  %>% mutate(replicate = row_number())
   protein_count_and_intensities = protein_count_and_intensities %>%
     group_by(GeneName, ProteinId) %>%
