fd17e057e03356fa70778e07434ecce103a8b18f
Brendan Spinks <brendanspinks@gmail.com>
2022-10-17 14:38:38 +1100

    Add EnrichmentBrowser to install.R

--- install.R
+++ install.R
@@ -1,6 +1,6 @@
 #!/usr/bin/Rscript
 
 install.packages("renv", repos = "http://cran.us.r-project.org")
-renv::install("bioc::Biobase")
+renv::install("bioc::Biobase", "bioc::EnrichmentBrowser")
 
 renv::install()
