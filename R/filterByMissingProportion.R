f61612c3420fb0969cd4634922790ce40d4d14a4
Anna Quaglieri <anna.quaglieri16@gmail.com>
2023-04-18 17:09:40 +1000

    run filter function; and setup function to run single enrichment comparison

---
+++ R/filterByMissingProportion.R
@@ -0,0 +1,29 @@
+#' filterByMissingProportion
+#'
+#' @param longIntensityDT
+#' @param conditionColname
+#' @param runIdColname
+#' @param idCol
+#' @param imputedColname
+#' @param minPropAvail
+#'
+#' @export filterByMissingProportion
+#'
+#' @import data.table
+filterByMissingProportion <- function(longIntensityDT,
+                                  conditionColname,
+                                  runIdColname,
+                                  idCol = "ID",
+                                  imputedColname = "Imputed",
+                                  minPropAvail = 0.5){
+    # how many replicates in each conditions
+    filterDT <- unique(longIntensityDT[, .(get(conditionColname), get(runIdColname))])[, .(max_count = .N), by = .(V1)]
+    filterDT <- merge(longIntensityDT, filterDT, by.x = conditionColname, by.y = "V1", all.x = T)
+    filterDT <- filterDT[get(imputedColname) == 0, .(count_rep = .N,
+                                                     max_count = max(max_count, na.rm = T)),
+                         by = c(conditionColname, idCol)][, repPC := count_rep/max_count]
+
+    isPresent <- filterDT[repPC >= minPropAvail, unique(get(idCol))]
+    finalFiltered <- longIntensityDT[get(idCol) %in% isPresent]
+    return(finalFiltered)
+}
