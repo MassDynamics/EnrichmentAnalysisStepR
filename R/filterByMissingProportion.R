d6b7078d4f82eebefaa61d41b9108f4836b358ee
Anna Quaglieri <anna.quaglieri16@gmail.com>
2023-04-21 15:46:32 +1000

    add filtering for too many missing values

--- R/filterByMissingProportion.R
+++ R/filterByMissingProportion.R
@@ -1,12 +1,4 @@
 #' filterByMissingProportion
-#'
-#' @param longIntensityDT
-#' @param conditionColname
-#' @param runIdColname
-#' @param idCol
-#' @param imputedColname
-#' @param minPropAvail
-#'
 #' @export filterByMissingProportion
 #'
 #' @import data.table
@@ -17,6 +9,7 @@ filterByMissingProportion <- function(longIntensityDT,
                                   imputedColname = "Imputed",
                                   minPropAvail = 0.5){
     # how many replicates in each conditions
+    longIntensityDT <- data.table(longIntensityDT)
     filterDT <- unique(longIntensityDT[, .(get(conditionColname), get(runIdColname))])[, .(max_count = .N), by = .(V1)]
     filterDT <- merge(longIntensityDT, filterDT, by.x = conditionColname, by.y = "V1", all.x = T)
     filterDT <- filterDT[get(imputedColname) == 0, .(count_rep = .N,
