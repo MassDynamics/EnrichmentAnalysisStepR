bfcedd9a0224d5e1adfef4063507f57ee8f9a368
Brendan Spinks <brendanspinks@gmail.com>
2023-08-15 14:11:19 +1000

    Try to install older version of bioconductor

--- install.R
+++ install.R
@@ -5,6 +5,11 @@
 
 install.packages(c('tidyr', 'dplyr', 'R.utils', 'rjson'), repos = 'https://cloud.r-project.org/')
 
+# We need to upgrade R to 4.3
 # https://support.bioconductor.org/p/9150992/#9150993
-BiocManager::install(version = "3.17")
-BiocManager::install("EnrichmentBrowser")
+# BiocManager::install(version = "3.17")
+# BiocManager::install("EnrichmentBrowser")
+
+# See older version install https://www.bioconductor.org/install/
+source("https://bioconductor.org/biocLite.R")
+BiocInstaller::biocLite(c("EnrichmentBrowser"))
