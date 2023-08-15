50e236cc9b115743c9a8d6ec68fa745b62fe9c67
Brendan Spinks <brendanspinks@gmail.com>
2023-08-15 10:00:36 +1000

    package ‘Bioconductor’ is not available for Bioconductor version '3.16'

--- install.R
+++ install.R
@@ -3,5 +3,8 @@
 # install.packages("devtools", repos = "http://cran.us.r-project.org")
 # devtools::install()
 
-install.packages(c('tidyr', 'dplyr', 'R.utils', 'rjson',"BiocManager"), repos = 'https://cloud.r-project.org/')
+install.packages(c('tidyr', 'dplyr', 'R.utils', 'rjson'), repos = 'https://cloud.r-project.org/')
+
+# https://support.bioconductor.org/p/9150992/#9150993
+BiocManager::install(version = "3.17")
 BiocManager::install("EnrichmentBrowser")
